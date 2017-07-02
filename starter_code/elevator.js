45a1a3667deeac565bc5f37e07191ef6d7726968
Manuel Valle <vallerem17@gmail.com>
2017-07-02 23:54:46 +0200

    Done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,82 +1,121 @@
 
+const chalk = require('chalk');
+
+console.log(chalk.blue('Looks like chalk does not work for some reason¿?¿?'));
+
+
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
+  constructor() {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
     this.waitingList = []; // array of objects (person)
     this.passengers = []; // array of objects (person)
-    this.requests   = []; // array of nums
-    this.direction  =  "Up";
+    this.requests = []; // array of numbers
+    this.direction = "Up";
   }
 
   start() {
     this.elevatorLoop = setInterval(() => this.update(), 1000);
   }
+
   stop() {
     clearInterval(this.elevatorLoop);
+    console.log("**** No more requests ****");
   }
+
   update() {
 
     let floor = this.floor;
-    let direction = this.direction;
+    let requests = this.requests;
 
+    this.waitingList.map((elem, index) => {
+      if (floor === elem.originFloor) {
+        this._passengersEnter(floor);
+      }
+    });
 
+    this.passengers.map((elem, index) => {
+      if (floor === elem.destinationFloor) {
+        this._passengersLeave(floor);
+      }
+    });
 
+    if (requests[0] >= floor) {
+      this.direction = "Up";
+    } else {
+      this.direction = "Down";
+    }
 
+    this.requests.forEach((elem, index) => {
+      if (this.floor === this.requests[index]) {
+        this.requests.splice(index, 1);
+      }
+    });
 
+    switch (this.direction) {
+      case "Up":
+        this.floorUp();
+        break;
+      case "Down":
+        this.floorDown();
+        break;
+      default:
+    }
 
-    this.log();
+    if (!requests.length) {
+       return this.stop();
+    }
+        this.log();
   }
+
+
   _passengersEnter(currentFloor) {
     this.waitingList.map((elem, index) => {
-      if(currentFloor === elem.originFloor){
+      if (currentFloor === elem.originFloor) {
         let enteringPerson = this.waitingList[index];
         this.passengers.push(enteringPerson);
-        this.waitingList.splice(index,1);
+        this.waitingList.splice(index, 1);
         this.requests.push(enteringPerson.destinationFloor);
-        this.requests.shift();
-        console.log(`${enteringPerson.name} just entered in the elevator.`);
+        console.log(`/*************/\n${enteringPerson.name} just entered in the elevator.\n/*************/`);
       }
     });
 
   }
   _passengersLeave(currentFloor) {
     this.passengers.map((elem, index) => {
-      if(currentFloor === elem.originFloor){
+      if (currentFloor === elem.destinationFloor) {
         let leavingPerson = this.passengers[index];
-        this.passengers.splice(index,1);
-        this.requests.shift();
-        console.log(`${leavingPerson.name} has left the elevator.`);
+        this.passengers.splice(index, 1);
+        // console.log(this.requests);
+        console.log(`/*************/\n      ${chalk.blue(leavingPerson.name)} has left the elevator.\n/*************/`);
       }
     });
   }
 
   floorUp() {
-    if(this.floor < 10){
+    if (this.floor < 10) {
       this.floor += 1;
-    } else{
+    } else {
       this.floor = 10;
     }
   }
 
   floorDown() {
-    if(this.floor > 0){
+    if (this.floor > 0) {
       this.floor -= 1;
-    } else{
+    } else {
       this.floor = 0;
     }
   }
-  
+
   call(person) {
     this.waitingList.push(person);
     this.requests.push(person.originFloor);
-    }
+  }
 
   log() {
     console.log(`Direction: ${this.direction} | Floor: ${this.floor}`);
   }
 }
-
-
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -3,16 +3,21 @@ const Person = require('./person.js');
 const Elevator = require('./elevator.js');
 
 let isa  = new Person("Isa",  0, 4);
-let raul = new Person("Raul", 2, 5);
-let fer  = new Person("Fer",  5, 1);
+let litz = new Person("Litz",  10, 2);
+let raul = new Person("Raul", 1, 5);
+let fer  = new Person("Fer",  3, 0);
 let gonzu = new Person("Gonzu",  7, 4);
+
 
 let elevator = new Elevator();
 
 elevator.start();
 
 elevator.call(isa);
-//elevator.call(raul);
-// elevator.call(fer);
+elevator.call(litz); // This one broke it. Not anymore  >:D
+elevator.call(raul);
+elevator.call(fer);
 
-//setTimeout(() => {elevator.call(gonzu)}, 10000);
+setTimeout(() => {
+  elevator.call(gonzu);
+}, 10000);
